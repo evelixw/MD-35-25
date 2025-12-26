52237f74f3f251ca1f9a817fcbd94a87a409053c
evelixw <kolevelina10@gmail.com>
2025-12-26 21:00:52 +0300

    added Module3 colors project

---
+++ Module03/Module03/Module03.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+enum class TrafficLight { RED, YELLOW, GREEN, UNKNOWN };
+
+void printLight(TrafficLight light) {
+    switch (light) {
+    case TrafficLight::RED:
+        cout << "Red" << endl;
+        break;
+    case TrafficLight::YELLOW:
+        cout << "Yellow" << endl;
+        break;
+    case TrafficLight::GREEN:
+        cout << "Green" << endl;
+        break;
+    case TrafficLight::UNKNOWN:
+        cout << "Unknown" << endl;
+        break;
+    }
+}
+
+int main() {
+    TrafficLight light = TrafficLight::UNKNOWN;
+    printLight(light);
+
+    string input;
+    cout << "Введите цвет светофора (red, yellow, green): ";
+    cin >> input;
+
+    TrafficLight userLight = TrafficLight::UNKNOWN;
+
+    if (input == "red") {
+        userLight = TrafficLight::RED;
+    }
+    else if (input == "yellow") {
+        userLight = TrafficLight::YELLOW;
+    }
+    else if (input == "green") {
+        userLight = TrafficLight::GREEN;
+    }
+    else {
+        cout << "Некорректный ввод! Будет использован цвет по умолчанию." << endl;
+    }
+
+    printLight(userLight);
+
+    return 0;
+}
