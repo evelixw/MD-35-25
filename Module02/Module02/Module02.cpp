f0daca16f681da702733caf647b6feefb7c736c4
evelixw <kolevelina10@gmail.com>
2025-12-26 20:30:31 +0300

    added Module2 project

---
+++ Module02/Module02/Module02.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите первое число (a): " << endl;
+    cin >> a;
+    cout << "Введите второе число (b): " << endl;
+    cin >> b;
+
+    cout << "Все операции" << endl;;
+
+    cout << "sizeof(a) = " << sizeof(a) << " байт" << endl;;
+    cout << "sizeof(b) = " << sizeof(b) << " байт" << endl;
+
+    cout << "Арифметические операции:" << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+
+    cout << "Логические операции:" << endl;
+    cout << boolalpha;
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+
+    cout << "Побитовые операции:" << endl;
+    cout << "a & b = " << (a & b) << endl;
+    cout << "a | b = " << (a | b) << endl;
+    cout << "a ^ b = " << (a ^ b) << endl;
+    cout << "~a = " << (~a) << endl;
+    cout << "a << 2 = " << (a << 2) << endl;
+    cout << "b >> 1 = " << (b >> 1) << endl;
+
+    return 0;
+}
